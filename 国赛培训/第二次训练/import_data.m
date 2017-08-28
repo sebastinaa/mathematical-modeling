d478ef8899e7b7b5df0dd9b383cb31d964ae6fbf
WangYang-wy <1070942929@qq.com>
2017-08-28 23:58:28 +0800

    learn staticial

--- 国赛培训/第二次训练/import_data.m
+++ 国赛培训/第二次训练/import_data.m
@@ -22,20 +22,20 @@ raw = raw(:,[3,4,5,6,7,8,9,10,11,12,13,14]);
 data = reshape([raw{:}],size(raw));
 
 %% 将导入的数组分配给列变量名称。
-Teacher = cellVectors(:,1); % 教师姓名
-Course = cellVectors(:,2); % 课程名
-C = data(:, 1); % 已评人数。
-D = data(:, 2); % 班级规模。
-E = data(:, 3); % 教学内容充实，注意推荐参考资料。
-F = data(:, 4); % 关注学科专业发展，能用最新科研成果充实、更新教学内容。
-G = data(:, 5); % 遵守教学纪律，按时上课，保证学时。
-H = data(:, 6); % 采用的教学方法能有效地启发学生思维，强调相关学科的思想，引导学生探究。
-I = data(:, 7); % 注意引导、激发学生对本课程的学习兴趣。
-J = data(:, 8); % 通过本课程学习，掌握了知识，开阔了视野，比学习之初更热爱本课程，收获大。
-K = data(:, 9); % 注意教学反馈，认真听取学生意见并加以改进。
-L = data(:, 10); % 教学认真负责，对学生要求严格，耐心解答疑问。
-M = data(:, 11); % 讲授娴熟，概念准确，条理清晰，重点突出，注意联系实际，引导学生学以致用。
-N = data(:, 12); % 合理使用板书和多媒体教学手段，有助于学生理解和掌握知识。
+Teacher = cellVectors(:,1);     % 教师姓名。
+Course = cellVectors(:,2);      % 课程名。
+C = data(:, 1);     % 已评人数。
+D = data(:, 2);     % 班级规模。
+E = data(:, 3);     % 教学内容充实，注意推荐参考资料。
+F = data(:, 4);     % 关注学科专业发展，能用最新科研成果充实、更新教学内容。
+G = data(:, 5);     % 遵守教学纪律，按时上课，保证学时。
+H = data(:, 6);     % 采用的教学方法能有效地启发学生思维，强调相关学科的思想，引导学生探究。
+I = data(:, 7);     % 注意引导、激发学生对本课程的学习兴趣。
+J = data(:, 8);     % 通过本课程学习，掌握了知识，开阔了视野，比学习之初更热爱本课程，收获大。
+K = data(:, 9);     % 注意教学反馈，认真听取学生意见并加以改进。
+L = data(:, 10);    % 教学认真负责，对学生要求严格，耐心解答疑问。
+M = data(:, 11);    % 讲授娴熟，概念准确，条理清晰，重点突出，注意联系实际，引导学生学以致用。
+N = data(:, 12);    % 合理使用板书和多媒体教学手段，有助于学生理解和掌握知识。
 
 % 特征的名称。
 featureNames = {
@@ -83,9 +83,8 @@ end
 
 MeanCourse = MeanCourse ./ NumberOfCourse; % 计算每种课程的平均值。
 
-%% 导出以上数据。
+%% 课程类型及其同类平均分表。
 Courses = ['课程名'; featureNames];
-
 Courses = [Courses'; [TypeOfCourse,num2cell(MeanCourse)]]; % 课程类型及其同类平均分。
 
 %% 计算每个老师教多少个班，多少人。
@@ -113,7 +112,7 @@ Teachers = {
 Teachers = [Teachers; [TeacherNames, num2cell([TeacherTeachStu, NumberClassTeacherTeach])]];
 
 %% 清除临时变量
-clearvars data raw cellVectors;
+% clearvars data raw cellVectors;
 
 %% 清楚所有变量
-clear;
+% clear;
